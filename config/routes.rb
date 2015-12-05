3849767afffd6a51c514ab4b7a1dd11a4b882d33
kathykate <kathy0kate@gmail.com>
2015-12-05 19:47:40 +0100

    user model finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root 'static_pages#home'
+  resources :users, only: [:index, :show]
+  
+  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
